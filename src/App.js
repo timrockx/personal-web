61cd631aa3072e5d5d8fcda358c974e44b09f2fa
timrockx <timothyyinlee@gmail.com>
2022-06-15 15:21:35 -0400

    updated app component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Menu from './components/Menu/Menu';
+import About from './components/About/About';
+import Education from './components/Education/Education';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<><Menu/><About/></>} />
+          <Route path="/education" element={<><Menu/><Education/></>} />
+        </Routes>
+      </BrowserRouter>
     </div>
+
+
+
+   
+
+    
+    
   );
 }
 
