082dbe476f2d5309be50addeb5507aeb77edcade
timrockx <timothyyinlee@gmail.com>
2022-06-19 14:14:56 -0400

    navbar on bottom, links to education and exp

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Menu.css'
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
-import { NavLink } from 'react-router-dom'
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import { HashLink } from 'react-router-hash-link'
 
 export default function Menu() {
   return (
@@ -9,25 +9,19 @@ export default function Menu() {
     <div className="nav-div">
         <Container>
 
-          <Navbar className="py-4 mx-auto">
+          <Navbar className="navbar-nav py-4 mx-auto" fixed="bottom">
   
-              <Navbar.Brand as={NavLink} to="/" className="fs-3">Timothy Lee</Navbar.Brand>
+              <Navbar.Brand as={HashLink} to="/" className="fs-3">Timothy Lee</Navbar.Brand>
               <Nav className="d-flex justify-content-end flex-grow-1" activeKey="/">                  
                   
-                  <Nav.Link as={NavLink} to="/education" className="nav-link fs-4">Education</Nav.Link>
-       
-                  
-                  <NavDropdown title="Portfolio" id="nav-dropdown">
-                      <NavDropdown.Item>
-                        <Nav.Link>Chase that Pump</Nav.Link>                      
-                      </NavDropdown.Item>
-                      <NavDropdown.Item>                        
-                        <Nav.Link href="#react-app">Spotify React App</Nav.Link>      
-                      </NavDropdown.Item>
-                      <NavDropdown.Item>
-                        <Nav.Link href="#investment-thesis">Investment Thesis</Nav.Link>                        
-                      </NavDropdown.Item>
-                  </NavDropdown>
+                  <Nav.Link as={HashLink} to="/background/#education" className="nav-link fs-4">Education</Nav.Link>
+
+                  <Nav.Link as={HashLink} to="/background/#experience" className="nav-link fs-4">Experience</Nav.Link>
+
+                  <Nav.Link as={HashLink} to="/background/#skills" className="nav-link fs-4">Skills</Nav.Link>
+
+                  <Nav.Link as={HashLink} to="/projects" className="nav-link fs-4">Projects</Nav.Link>
+
               </Nav>
           </Navbar>
         
