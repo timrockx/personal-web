e5f890467df97a9189ea366f059049ef5f50905f
timrockx <timothyyinlee@gmail.com>
2022-06-19 14:13:51 -0400

    content changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -55,7 +55,7 @@ export default function About() {
 
   return (
     <div className="intro">
-        <Container style={{width: "60%", border: "1px solid black", borderRadius: "25px", padding: "20px"}}>
+        <Container style={{width: "60%", padding: "10px"}}>
         
             <div className="question messages">
                 <div className="message last">
@@ -66,7 +66,7 @@ export default function About() {
             <div className="answer messages">
                 <div className="message last">
                     <p className="text">
-                        My name is Tim, and I'm an aspiring Software Developer with a huge passion
+                        My name is Timothy Lee, and I'm an aspiring Software Developer with a huge passion
                         in tech and finance. I'm currently in the Computer Science & Business (CSB) program
                         at Lehigh University, with plans to graduate in May 2024.
                     </p>
@@ -84,8 +84,8 @@ export default function About() {
             <div className="answer messages">
                 <div className="message last">
                     <p className="text">
-                        I'm currently working as a Software Developer at a Legal AI startup 
-                        based out of Princeton University. Looking ahead, I'm planning to pursue a 
+                        I'm working as a Software Development Intern at Claudius Scholar, a Legal AI 
+                        startup based out of Princeton University. Looking ahead, I'm planning to pursue a 
                         career in tech consulting, with an end goal of VC and Product Management.
                     </p>
                 </div>
